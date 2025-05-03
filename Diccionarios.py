f18d2cd5a295c478cb4684ef0694f295f00aefa7
LeidyEst <irladysestrada05@gmail.com>
2025-05-02 20:00:23 -0500

    Add files via upload

---
+++ Diccionarios.py
@@ -0,0 +1,15 @@
+#diccionario 
+Cantantes ={
+    "Nombre": "Hyde",
+    "Edad":46,
+    "Banda":"Laruku",}
+
+Cantantes["Banda"] = "Vampire"
+
+
+#eliminar elementos
+
+#recorren un diccionario
+for key, value in Cantantes.items():
+    print(key, value)
+
